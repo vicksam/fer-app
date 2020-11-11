3bedd44ded645198af9ae63caf4dd17b378120cf
vicksam <vicksam@zoho.com>
2020-11-11 09:29:12 +0100

    Add face detector

--- app/src/main/java/com/vicksam/ferapp/MainActivity.kt
+++ app/src/main/java/com/vicksam/ferapp/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.vicksam.ferapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Size
+import androidx.appcompat.app.AppCompatActivity
+import com.otaliastudios.cameraview.Facing
+import husaynhakeem.io.facedetector.FaceDetector
+import husaynhakeem.io.facedetector.Frame
+import husaynhakeem.io.facedetector.LensFacing
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lensFacing =
+                savedInstanceState?.getSerializable(KEY_LENS_FACING) as Facing? ?: Facing.BACK
+        setupCamera(lensFacing)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewfinder.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewfinder.stop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putSerializable(KEY_LENS_FACING, viewfinder.facing)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewfinder.destroy()
+    }
+
+    private fun setupCamera(lensFacing: Facing) {
+        val faceDetector = FaceDetector(faceBoundsOverlay)
+        viewfinder.facing = lensFacing
+        viewfinder.addFrameProcessor {
+            faceDetector.process(
+                    Frame(
+                            data = it.data,
+                            rotation = it.rotation,
+                            size = Size(it.size.width, it.size.height),
+                            format = it.format,
+                            lensFacing = if (viewfinder.facing == Facing.BACK) LensFacing.BACK else LensFacing.FRONT
+                    )
+            )
+        }
+
+        toggleCameraButton.setOnClickListener {
+            viewfinder.toggleFacing()
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val KEY_LENS_FACING = "key-lens-facing"
     }
 }
